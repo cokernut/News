a025f7dd92c8bb1ff107921f7f46e6f05da5e42c
Cokernut <981100707@qq.com>
2016-06-08 17:57:07 +0800

    添加版本号

--- app/src/main/java/top/cokernut/news/activity/AboutActivity.java
+++ app/src/main/java/top/cokernut/news/activity/AboutActivity.java
@@ -3,6 +3,9 @@ package top.cokernut.news.activity;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import top.cokernut.news.R;
 import top.cokernut.news.base.BaseActivity;
@@ -10,6 +13,7 @@ import top.cokernut.news.base.BaseActivity;
 public class AboutActivity extends BaseActivity {
 
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private TextView mVersionTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,22 @@ public class AboutActivity extends BaseActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         mCollapsingToolbarLayout.setTitle(getString(R.string.about_title));
+        mVersionTV = (TextView) findViewById(R.id.tv_version);
+        mVersionTV.setText("0.1.1");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
